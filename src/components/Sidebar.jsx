9f739e42f679c0c98c7cdebe5eb748a7a5c0b153
lexeach <80155340+lexeach@users.noreply.github.com>
2026-06-08 18:28:44 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -112,7 +112,7 @@ export default function Sidebar() {
                             textTransform: 'uppercase', 
                             letterSpacing: '0.04em',
                             // एक्टिव होने पर हल्का डार्क ग्रे, वरना एकदम सॉफ्ट लाइट ग्रे
-                            background: isSubModuleOpen ? '#e2e8f0' : '#87ceeb', 
+                            background: isSubModuleOpen ? '#e9bd9a' : '#87ceeb', 
                             cursor: 'pointer',
                             display: 'flex',
                             justifyContent: 'space-between',
