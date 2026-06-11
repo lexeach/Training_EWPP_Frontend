32b04d06b7f36b573b36be1272c12d60e8eef59f
lexeach <80155340+lexeach@users.noreply.github.com>
2026-06-11 17:54:37 +0530

    Create LandscapeAlert.jsx

---
+++ src/components/LandscapeAlert.jsx
@@ -0,0 +1,6 @@
+// src/components/LandscapeAlert.jsx
+import React from 'react';
+
+export default function LandscapeAlert() { 
+  return null; // यह स्क्रीन पर कुछ भी रेंडर नहीं करेगा (पूरी तरह खाली रहेगा)
+}
